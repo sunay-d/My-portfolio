e4bafd194416eef6dd35367ffc3023739b1a4724
Sunay <84284117+sunay-d@users.noreply.github.com>
2023-01-27 21:24:03 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -15,6 +15,14 @@ export const projects = [
       image: "assets/images/password.jpg",
       link: "https://sunay-d.github.io/passwordgenerator/index",
     },
+    {
+      title: "Image Editor",
+      subtitle: "HTML, CSS and Vanilla JS",
+      description:
+        "This is an image editor app. It allows user to upload images, change their brightness, contrasts and other settings before download them back.",
+      image: "assets/images/image_editor.jpg",
+      link: "https://sunay-d.github.io/image_editor/imageEditor",
+    },
   ];
 
   export const skills = ["HTML5","CSS3","JavaScript","Python","SQL","React","Tailwind CSS","Git"]
